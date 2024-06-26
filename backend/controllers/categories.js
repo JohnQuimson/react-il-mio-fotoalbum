717c10c3064f61f8a3aa6e7b0207184e1b6fb27f
JohnQuimson <john.quimson777@gmail.com>
2024-06-26 14:47:16 +0200

    feat: update category controller

--- backend/controllers/categories.js
+++ backend/controllers/categories.js
@@ -38,7 +38,7 @@ const show = async (req, res) => {
     });
 
     if (!category) {
-      return res.status(404).json({ error: 'Nessuna categoria trovata.' });
+      return res.status(404).json({ error: 'Nessuna categoria trovata' });
     }
 
     res.status(200).json(category);
@@ -47,7 +47,36 @@ const show = async (req, res) => {
   }
 };
 
-const update = async (req, res) => {};
+const update = async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  try {
+    const existingCategory = await prisma.category.findUnique({
+      where: {
+        id: parseInt(id),
+      },
+    });
+
+    if (!existingCategory) {
+      return res.status(404).json({ error: 'Nessuna categoria trovata' });
+    }
+
+    const updatedCategory = await prisma.category.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        name: name || existingCategory.name,
+      },
+    });
+
+    res.status(200).json(updatedCategory);
+  } catch (err) {
+    errorHandler(err, req, res);
+  }
+};
+
 const destroy = async (req, res) => {};
 
 module.exports = {
