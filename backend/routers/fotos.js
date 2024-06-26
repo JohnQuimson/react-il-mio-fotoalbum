ec4ba4fa7d439a110c1605494ca54d3bb12e7538
JohnQuimson <john.quimson777@gmail.com>
2024-06-26 10:51:16 +0200

    feat: router & controller

---
+++ backend/routers/fotos.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+
+const {
+  store,
+  index,
+  show,
+  update,
+  destroy,
+} = require('../controllers/fotos.js');
+
+router.get('/', index);
+router.use('/:id', show);
+router.post('/', store);
+router.put('/:id', update);
+router.delete('/:id', destroy);
+
+module.exports = router;
