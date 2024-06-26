c9a0c0ec32204170499da0e835548926c8f91717
JohnQuimson <john.quimson777@gmail.com>
2024-06-26 11:34:37 +0200

    feat: error middlewares

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const app = express();
 
+const errorHandler = require('./middlewares/errorHandler.js');
+const notFound = require('./middlewares/notFound.js');
+
+const fotoRouter = require('./routers/fotos.js');
+
 require('dotenv').config();
 const { PORT, HOST } = process.env;
 const port = PORT || 3000;
+
+app.use('/fotos', fotoRouter);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log(`Server attivo su ${HOST}:${port}`);
