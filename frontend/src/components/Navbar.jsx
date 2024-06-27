7e1e92a3196b6931a7ca5ecc2d7d979f5d8b236e
JohnQuimson <john.quimson777@gmail.com>
2024-06-27 14:23:47 +0200

    feat: minimal style navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -18,14 +18,15 @@ const urlPages = [
 export default function () {
   return (
     <header>
-      <nav>
-        <menu>
+      <nav className="d-flex justify-content-between">
+        <div className="cont-logo">Logo</div>
+        <ul className="d-flex justify-content-around list-unstyled ">
           {urlPages.map(({ label, href }, i) => (
-            <li key={`urlPage${i}`}>
+            <li key={`urlPage${i}`} className="mx-2">
               <NavLink to={href}>{label}</NavLink>
             </li>
           ))}
-        </menu>
+        </ul>
       </nav>
     </header>
   );
