f6b7996256198045a7988574e07b5d9a3dd8e091
JohnQuimson <john.quimson777@gmail.com>
2024-06-27 14:41:25 +0200

    feat: fotoCard

---
+++ frontend/src/components/FotoCard.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useRef, useState } from 'react';
+
+export default function ({ id, title, description, img, visible, categories }) {
+  return (
+    <>
+      <div className="foto-container">
+        <div className={`foto ${visible ? 'visible' : ''}`}>
+          {/* Img */}
+          <div className="card-image">
+            <img src={img} alt="foto Random" />
+          </div>
+          <div className="card-content">
+            <h3>{title}</h3>
+            {categories.length > 0 ? (
+              <div className="categories">
+                <strong>categories:</strong>
+                <ul>
+                  {categories.map((category, index) => (
+                    <li key={`ingr${index}`}>{category}</li>
+                  ))}
+                </ul>
+              </div>
+            ) : (
+              <strong>Categorie non specificati</strong>
+            )}
+            <p>{description}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
