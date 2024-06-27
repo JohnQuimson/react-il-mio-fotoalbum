1ed50f9d7ab1f9d82923062bc22470fcd02c8ac4
JohnQuimson <john.quimson777@gmail.com>
2024-06-27 16:30:47 +0200

    feat: private page middlewares

---
+++ frontend/src/middlewares/PrivatePage.jsx
@@ -0,0 +1,10 @@
+import { Navigate } from 'react-router-dom';
+import { useAuth } from '../contexts/AuthContext';
+
+export default function ({ children }) {
+  const { isLoggedIn } = useAuth();
+
+  if (!isLoggedIn) return <Navigate to="/login" />;
+
+  return children;
+}
