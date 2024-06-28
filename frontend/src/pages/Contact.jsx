4ffe616b82aa09611a9c93545c28b9bcaddbe23f
JohnQuimson <john.quimson777@gmail.com>
2024-06-28 17:16:09 +0200

    feat: contact style

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -23,59 +23,61 @@ export default function () {
     try {
       const response = await axios.post('/messages', formData);
       console.log('Messaggio inviato con successo:', response.data);
-      navigate('/fotos');
+      navigate('/');
     } catch (error) {
       console.error("Errore durante l'invio del messaggio:", error);
     }
   };
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Nome:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="name">Cognome:</label>
-          <input
-            type="text"
-            id="surname"
-            name="surname"
-            value={formData.surname}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="email">Email:</label>
-          <input
-            type="email"
-            id="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="message">Messaggio:</label>
-          <textarea
-            id="content"
-            name="content"
-            value={formData.content}
-            onChange={handleChange}
-            required
-          />
-        </div>
-        <button type="submit">Invia Messaggio</button>
-      </form>
+      <section id="contacts">
+        <form onSubmit={handleSubmit} className="border">
+          <div className="cont-info">
+            <label htmlFor="name">Nome:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="cont-info">
+            <label htmlFor="name">Cognome:</label>
+            <input
+              type="text"
+              id="surname"
+              name="surname"
+              value={formData.surname}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="cont-info">
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <div className="cont-info">
+            <label htmlFor="message">Messaggio:</label>
+            <textarea
+              id="content"
+              name="content"
+              value={formData.content}
+              onChange={handleChange}
+              required
+            />
+          </div>
+          <button type="submit">Invia Messaggio</button>
+        </form>
+      </section>
     </>
   );
 }
