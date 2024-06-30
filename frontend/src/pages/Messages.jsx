013eb79d5acbda3705ad6bfd79ab6805c069927b
JohnQuimson <john.quimson777@gmail.com>
2024-07-01 01:41:18 +0200

    feat: messages page

--- frontend/src/pages/Messages.jsx
+++ frontend/src/pages/Messages.jsx
@@ -1,33 +1,46 @@
 import { useEffect, useState } from 'react';
 import axios from '../utils/axiosClient';
 
-export default function () {
+export default function Messages() {
   const [messages, setMessages] = useState(null);
 
   useEffect(() => {
     axios.get('/messages').then(({ data }) => {
       setMessages(data);
     });
-  });
+  }, []);
 
   return (
-    <>
-      <section id="messages">
-        {messages === null ? (
-          <p>Loading...</p>
-        ) : (
-          <ul className="row m-0 p-0">
-            {messages.map((m) => (
-              <li
-                key={`foto${m.id}`}
-                className="col-12 col-sm-6 col-md-4 col-xl-3 g-4"
-              >
-                {m.name}
-              </li>
-            ))}
-          </ul>
-        )}
-      </section>
-    </>
+    <section id="messages">
+      <h1 className="text-center">Messaggi</h1>
+      {messages === null ? (
+        <p>Loading...</p>
+      ) : (
+        <div className="mess-form-cont">
+          <table className="custom-table">
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Name</th>
+                <th>Surname</th>
+                <th>Email</th>
+                <th>Content</th>
+              </tr>
+            </thead>
+            <tbody>
+              {messages.map((m) => (
+                <tr key={`message${m.id}`}>
+                  <td>{m.id}</td>
+                  <td>{m.name}</td>
+                  <td>{m.surname}</td>
+                  <td>{m.email}</td>
+                  <td>{m.content}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      )}
+    </section>
   );
 }
